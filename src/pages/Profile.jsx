5388584370830390160a75511db35b9f0d9bc54a
Arvind Keshri <arvidce@gmail.com>
2024-08-19 06:00:08 +0530

    on refresh loading fill-data on profile page from firebase

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const Profile = () => {
@@ -7,6 +7,25 @@ export const Profile = () => {
   const [url, setUrl] = useState("");
 
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fillData = async () => {
+      try {
+        const idToken =
+          "eyJhbGciOiJSUzI1NiIsImtpZCI6ImQ0MjY5YTE3MzBlNTA3MTllNmIxNjA2ZTQyYzNhYjMyYjEyODA0NDkiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vcmVhY3QtYXV0aC1jNjY3ZiIsImF1ZCI6InJlYWN0LWF1dGgtYzY2N2YiLCJhdXRoX3RpbWUiOjE3MjQwMjY3NjksInVzZXJfaWQiOiJuUlJLYUJDYllJVEl3SHpGWDBuZmZKQUhSTzcyIiwic3ViIjoiblJSS2FCQ2JZSVRJd0h6RlgwbmZmSkFIUk83MiIsImlhdCI6MTcyNDAyNjc2OSwiZXhwIjoxNzI0MDMwMzY5LCJlbWFpbCI6ImFydmlkY2UyQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJhcnZpZGNlMkBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.a8noSIWqJe3xLXk2Ia519IRZsJHeH41MF68DJ7-zdBv0ZX5RpCBm3i8ljA1DzQf8MKtCAhc9TB3kfqv7ZqaOYIpgN-r2UH4VzOitjUkYNsBoPH6dPmmNq06VQWITDnr5FiHQaZm26P-f9vzKfG0GWJfVyakomG-3x92R_jlppfUXYLQAwIYfwvDhS2imqtIui83y4XlJ4GpwOWlWY1PMQFE1LB_FxNVeL7GWzDWha4O7phejflXCxWTbp3RvpM_erQehWRYn2MgnrFLEwecBoEOyPUVWVwMf7HwnMLKVyYwYvh2tj4blQM6OKsMBhXYR3iTyBQBT__OcsyN3Sc8n1A";
+        const res = await axios.post(
+          "https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyDB_xv6dB9fyCD97pI4UeFDSFuz-qig94A",
+          { idToken },
+          { headers: { "Content-Type": "application/json" } }
+        );
+        const userData = res.data.users[0];
+        console.log("onrefresh", res.data, userData);
+      } catch (error) {
+        console.error("Not able to get fillData", error);
+      }
+    };
+    fillData();
+  }, []);
 
   const nameInputHandler = (e) => {
     console.log("name input");
@@ -34,8 +53,8 @@ export const Profile = () => {
       );
 
       console.log("user data updated successfully", res.data);
-    } catch {
-      console.error("error updating data", obj);
+    } catch (err) {
+      console.error("error updating data", err);
     }
   };
 
